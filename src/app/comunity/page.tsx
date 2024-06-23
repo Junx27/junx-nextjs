2e44ab2ef8eb9fa5f56bde21aa20b30e7952b4b0
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2024-06-23 18:04:04 +0700

    test github

--- src/app/comunity/page.tsx
+++ src/app/comunity/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="mx-64 mb-32">
+      <h1 className="m-32">
+        Ini adalah halaman komuniti dimana saya membahasa tentang sebuah
+        komuniti saya
+      </h1>
+      <p className="mb-20">
+        ini adalah paragraf dengan menggunakan sebuah next js di dalamanya.
+        dimana saya dapat membuat sebuah deskripsi dengan menggunakan tag html p
+      </p>
+      <p>
+        saya akan mendeploy dengan menggunakan gihub sebagai repositori dan
+        menggunakan vercel sebagai server gratis dari pengembang next js
+      </p>
+    </div>
+  );
 }
 
 export default page;
