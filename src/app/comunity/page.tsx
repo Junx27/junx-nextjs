4c2e81582a641dcf04874aceefb5626498547d55
Tri Saptono <103058236+Junx27@users.noreply.github.com>
2024-06-30 18:40:54 +0700

    test

--- src/app/comunity/page.tsx
+++ src/app/comunity/page.tsx
@@ -1,9 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import React from "react";
 
 function page() {
   return (
-    <div className="mx-64 mb-32">
-      <h1 className="font-black text-5xl">testing</h1>
+    <div className="mx-32 grid grid-cols-3 gap-20">
+      <div className="peer relative group overflow-hidden rounded-lg">
+        <div className="w-full h-96">
+          <img
+            src="https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-rainbow-curves-abstract-colorful-background-image_2164067.jpg"
+            alt=""
+            className="transition-transform duration-500 w-full h-full object-cover group-hover:scale-125 group-hover:translate-y-10"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/20"></div>
+          <div className="transition-all duration-700 absolute inset-0 translate-x-32 translate-y-20 opacity-0 group-hover:translate-y-10 group-hover:opacity-100">
+            <h1 className="capitalize mt-32 bg-orange-500 p-2 rounded-full w-20 text-center">
+              test
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="peer relative group overflow-hidden rounded-lg">
+        <div className="w-full h-96">
+          <img
+            src="https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-rainbow-curves-abstract-colorful-background-image_2164067.jpg"
+            alt=""
+            className="transition-transform duration-500 w-full h-full object-cover group-hover:scale-125 group-hover:translate-y-10"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/20"></div>
+          <div className="transition-all duration-700 absolute inset-0 translate-x-32 translate-y-20 opacity-0 group-hover:translate-y-10 group-hover:opacity-100">
+            <h1 className="capitalize mt-32 bg-orange-500 p-2 rounded-full w-20 text-center">
+              test
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="peer relative group overflow-hidden rounded-lg">
+        <div className="w-full h-96">
+          <img
+            src="https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-rainbow-curves-abstract-colorful-background-image_2164067.jpg"
+            alt=""
+            className="transition-transform duration-500 w-full h-full object-cover group-hover:scale-125 group-hover:translate-y-10"
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/20"></div>
+          <div className="transition-all duration-700 absolute inset-0 translate-x-32 translate-y-20 opacity-0 group-hover:translate-y-10 group-hover:opacity-100">
+            <h1 className="capitalize mt-32 bg-orange-500 p-2 rounded-full w-20 text-center">
+              test
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/price/page.tsx
+++ src/app/price/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col gap-20 mx-32">
+      <div className="bg-[url('public/portofolio.png')]"></div>
+      <div className="peer group bg-green-400 h-32 w-32 hover:bg-purple-400"></div>
+      <div className="transition-all duration-700 peer-hover:[transform:rotateX(180deg)] bg-green-400 h-32 w-32 tra"></div>
+      <div className="transition-all duration-700 peer-hover:[transform:rotateY(180deg)] [background-color:red] [transform-style:preserve-3d] h-32 w-32"></div>
+    </div>
+  );
 }
 
 export default page;
